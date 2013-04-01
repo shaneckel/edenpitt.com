b11d5a7b87c212e5a34de7dd2872bcbdf7a9f6be
shane eckel <shaneckel@gmail.com>
2013-04-01 00:21:15 -0400

    space for testing

--- index.php
+++ index.php
@@ -9,7 +9,8 @@
   	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
   	<title>Eden | Fresh. Local. Smart. | 735 Copeland St. - Pittsburgh, Pa</title>
- 	<meta name="description" content="">
+ 		<meta name="description" content="">
+
 
   	<meta name="viewport" content="width=device-width,initial-scale=1">
 	<script>//code healthy</script>
