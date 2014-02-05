e9b373c68e844c684504c5eb225d3725e3f122fd
shane eckel <shaneckel@gmail.com>
2014-02-05 01:08:12 -0500

    comasdsadsda

--- process.php
+++ process.php
@@ -10,26 +10,24 @@ if( isset($_POST) ){
   $email    = $_POST['email'];  
 
 
-  if (empty($_POST['name']))
+  if (empty($name)
     $errors['name'] = 'Name is required.';
 
-  if (empty($_POST['email']))
+  if (empty($email)
     $errors['email'] = 'Please include an email.';
 
-  if (empty($_POST['message']))
+  if (empty($message)
     $errors['message'] = 'Please include a message.';
 
   if (!empty($errors)) {
     $headers = "Edenpitt.com email from " . $name .  ".\r\n";
     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
     
-    $emailbody = "<h2>Hey Hilary.</h2>
-                  <h3>{$name} Has sent you an email.</h3>
-                  <p>{$message}</p>
-                  <p>Their email is:</p>
-                  <h2>{$email}</h2> 
-                  <p>:)</p>"
-
+    $emailbody = "<h2>Hey Hilary.</h2>";
+    $emailbody .= "<h3>". $name . "Has sent you an email.</h3>";
+    $emailbody .= "<p>" . $message . "</p><p>Their email is:</p>"
+    $emailbody .= "<h2>" . $email . "</h2><p>:)</p>"
+ 
     mail("shaneckel@gmail.com","EdenPitt Email from " . $name . ".",$emailbody,$headers);
 
     $data['success'] = true;
