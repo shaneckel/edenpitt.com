d4af5427dcd24db9c8ece264f70fa0299d895b70
shane eckel <shaneckel@gmail.com>
2014-02-05 01:16:06 -0500

    seriousloy man

--- process.php
+++ process.php
@@ -4,31 +4,37 @@ if( isset($_POST) ){
   
   $errors   = array();
   $data     = array();  
-  
+  $formok   = true;
+
   $name     = $_POST['name'];
   $message  = $_POST['message'];
   $email    = $_POST['email'];  
 
-
-  if (empty($name))
+  if (empty($name)){
     $errors['name'] = 'Name is required.';
-
-  if (empty($email))
+    $formok = false;
+  }
+  if (empty($email)){
     $errors['email'] = 'Please include an email.';
-
-  if (empty($message))
+    $formok = false;
+  }
+  if (empty($message)){
     $errors['message'] = 'Please include a message.';
-
-  if (!empty($errors)) {
+    $formok = false;
+  }
+  if ($formok) {
     $headers = "Edenpitt.com email from " . $name .  ".\r\n";
     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
     
-    $emailbody = "<h2>Hey Hilary.</h2>";
-    $emailbody .= "<h3>". $name . "Has sent you an email.</h3>";
-    $emailbody .= "<p>" . $message . "</p><p>Their email is:</p>";
-    $emailbody .= "<h2>" . $email . "</h2><p>:)</p>";
+    $emailbody = "<p>Eden Email Form</p>
+            <p><strong>Name: </strong> {$name} </p>
+            <p><strong>Email Address: </strong> {$email} </p>
+            <p><strong>Message: </strong> {$message} </p>
+            <p>This message was sent from the IP Address: </p>";
  
-    mail("shaneckel@gmail.com","EdenPitt Email from " . $name . ".",$emailbody,$headers);
+    $emailMessage = "EdenPitt Email from {$name}.";
+
+    mail("shaneckel@gmail.com",$emailMessage,$emailbody,$headers);
 
     $data['success'] = true;
     $data['message'] = 'Your email was successfully sent on '. date('l jS \of F Y h:i:s A');
