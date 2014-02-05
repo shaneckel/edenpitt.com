f782dd6aa0eaad81c8c3d5849384133331da13ac
shane eckel <shaneckel@gmail.com>
2014-02-05 15:55:03 -0500

    cartharsis

--- process.php
+++ process.php
@@ -9,30 +9,29 @@ if( isset($_POST) ){
   $message  = $_POST['message'];
   $email    = $_POST['email'];  
 
-  if (empty($_POST['name'])){
+  if (empty($name) && isset($name)){
     $errors['name'] = 'Name is required.';
-   }
+  }
 
-  if (empty($_POST['email'])){
+  if (empty($email) && isset($email)){
     $errors['email'] = 'Please include an email.';
-   }
+  }
 
-  if (empty($_POST['message'])){
+  if (empty($message) && isset($message)){
     $errors['message'] = 'Please include a message.';
-   }
+  }
 
-  if (!empty($errors)) {
+  if (empty($errors)) {
     $headers = "From: Eden Pitt <hello@edenpitt.com>" . "\r\n";
     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
     $headers .= 'MIME-Version: 1.0' . "\r\n";
  
-    $emailbody = "<p>Eden Email Form</p>
-            <p><strong>Name: </strong> {$name} </p>
-            <p><strong>Email Address: </strong> {$email} </p>
-            <p><strong>Message: </strong> {$message} </p>
-            <p>This message was sent from the IP Address: </p>";
+    $emailbody = "<h1 style='font-size:18px;'>{$name} sent you an email.</h1>
+                  <p style='border-top:1px solid #333;padding-top:20px;margin-top:10px;'>{$message} </p>
+                  <p style='border-top:1px solid #333;padding-top:10px;margin-top:10px;'>Their return email is:</p>
+                  <h2 style='font-size:16px;'>{$email}</h2>";
  
-    $emailMessage = "EdenPitt Email.";
+    $emailMessage = "Eden email from {name}.";
 
     mail("shaneckel@gmail.com",$emailMessage,$emailbody,$headers);
 
