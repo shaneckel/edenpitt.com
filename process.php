817339694bde78948162d4704620a573d2e955a5
shane eckel <shaneckel@gmail.com>
2014-02-05 17:11:19 -0500

    getting done

--- process.php
+++ process.php
@@ -8,7 +8,7 @@ if( isset($_POST) ){
   $data = array();  
  
   $name = $_POST['name'];
-  $message = $_POST['message'];
+  $message = stripslashes($_POST['message']);
   $email = $_POST['email'];  
 
   if (empty($_POST['name']) ){
@@ -31,14 +31,14 @@ if( isset($_POST) ){
     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
     $headers .= 'MIME-Version: 1.0' . "\r\n";
  
-    $emailbody = "<h1 style='font-size: 30px; color: #ed1c24; padding: 20px; font-family: sans-serif;>Edenpitt.com</h1><h1 style='font-size:26px; padding: 20px; background: #ed1c24; color: white; font-family: sans-serif; font-weight: normal;'><span style='font-weight:bold'>{$name}</span> sent you an email</h1><p style='font-size: 16px; padding:30px 20px;'>{$message}</p><h2 style='padding: 20px; font-size: 18px; background: #ed1c24; color: white; font-family: sans-serif;'>Their return email is. <strong>{$email}</strong>.</h2>";
+    $emailbody = "<h1 style='font-size: 30px; color: #ed1c24; padding: 20px; font-family: sans-serif;'>Edenpitt.com</h1><h1 style='font-size:26px; padding: 20px; background: #ed1c24; color: white; font-family: sans-serif; font-weight: 300;'><span style='font-weight:900'>{$name}</span> sent you an email</h1><p style='font-size: 16px; padding:30px 20px;'>{$message}</p><h2 style='padding: 20px; font-size: 18px; background: #ed1c24; color: white; font-family: sans-serif;'>Their return email is. <strong style='color: white;'>{$email}</strong>.</h2>";
  
     $emailMessage = "Edenpitt.com Email";
 
     mail("shaneckel@gmail.com",$emailMessage,$emailbody,$headers);
 
     $data['success'] = true;
-    $data['message'] = 'Your email was successfully sent on '. date('l jS \of F Y h:i:s A');
+    $data['message'] = 'Your email was successfully sent at '. date('h:i A') . '.';
  
   }else{
     $data['success'] = false;
